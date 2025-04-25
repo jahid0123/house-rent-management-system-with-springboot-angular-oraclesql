2d94d3efc38cb349f7b9d29934ada6719418c629
Jahid Ahmed <ajahid97@gmail.com>
2025-04-25 15:19:07 +0600

    updated with component

--- Frontant/house-rent-management-system/src/app/app.routes.ts
+++ Frontant/house-rent-management-system/src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { AboutComponent } from './page/about/about.component';
 import { HomeComponent } from './page/home/home.component';
 import { AddPropertyComponent } from './page/form/add-property/add-property.component';
 import { ViewDetailsComponent } from './page/view/view-details/view-details.component';
+import { ContactUsComponent } from './page/section/footer-content/contact-us/contact-us.component';
+import { AboutUsComponent } from './page/section/footer-content/about-us/about-us.component';
 
 export const routes: Routes = [
 
@@ -18,5 +20,7 @@ export const routes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'add-property', component: AddPropertyComponent},
     {path: 'view-details', component: ViewDetailsComponent},
+    {path: 'contact-us', component: ContactUsComponent},
+    {path: 'about-us', component: AboutUsComponent},
     {path: '**', redirectTo: 'home'}
 ];
---
+++ Frontant/house-rent-management-system/src/app/page/section/footer-content/contact-us/contact-us.component.ts
@@ -0,0 +1,12 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  imports: [NgIf],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.css'
+})
+export class ContactUsComponent {
+
+}
